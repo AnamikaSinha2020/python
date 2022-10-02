a47708803b565e07d73e0edbf70befa847d22190
Anamika Sinha <aniverma2006@gmail.com>
2022-10-02 17:02:20 -0400

    first commit

---
+++ ex13.py
@@ -0,0 +1,9 @@
+print("How old are you?", end=" ")
+age=input()
+print("how much do you weugh ",end =" ")
+weight =input()
+
+print(f"so, you're {age} year old and { weight} ponds heavy")
+
+print("your name ",end=" ")
+name= input()
