b11b913053ba2132b2332812848dfbf93d4cf392
Anamika Sinha <aniverma2006@gmail.com>
2022-12-13 16:32:30 -0500

    Find a sqrt of number

---
+++ ex6.py
@@ -0,0 +1,14 @@
+#Python Program to calculate the square root
+import math
+import cmath
+
+
+print("Enter a number")
+a=int(input())
+sqrt1 =math.sqrt(a)
+print(sqrt1)
+
+b=16+4j
+
+sqrt2=cmath.sqrt(b)
+print(sqrt2)
