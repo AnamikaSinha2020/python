811857ac1a950654a0f54415bd3fd8f63bcad6ea
Anamika Sinha <aniverma2006@gmail.com>
2023-01-09 18:28:22 -0500

    loop through dictonary

---
+++ Ex20.py
@@ -0,0 +1,9 @@
+dict={'name':"Anamika",
+      'email':"anihdk@gmail.com",
+      "phone":3526338293
+      }
+# print(dict.items())
+# print(dict.keys())
+# print(dict.values())
+for keys,values in dict.items():
+    print(keys,values)
