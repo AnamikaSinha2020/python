a5ca019408dd64afc98f98c863f90d4f5e6b29ec
Anamika sinha <61617222+AnamikaSinha2020@users.noreply.github.com>
2023-01-16 16:36:51 -0500

    studen avg marks, list and dict implementation

---
+++ ex22.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+def avg(student_marks):
+    sum = 0
+    avg = 0.0
+    for i in line:
+        sum += i
+
+    avg = sum / len(line)
+    return avg
+
+
+
+
+    query_name = input()
+    if query_name ==student_marks[name] :
+        print()
